366d522bade673176ef188a3886e509660c5f480
rahulwarad123 <rahul.warad@gmail.com>
2017-05-08 17:55:51 +0530

    updated with agent code

--- apps/aos/aos.js
+++ apps/aos/aos.js
@@ -33,6 +33,11 @@ var URL_RENTERS_RESIDENCEINFO = URL_RENTERS_BASE + "/renters/residence-informati
 var URL_RENTERS_ORDERQUOTE = URL_RENTERS_BASE + "/renters/quote";
 
 var FROM_EMAIL_ID = "pgoud@gmail.com";
+var AGENTFINDRESP = [
+    "Sure. what's your zip code?",
+    "I can help you with that. What's your zip?",
+    "Please provide the zip?",
+];
 var EMAILRESP = [
     "Sure. what's your email id?",
     "Please provide the email id",
@@ -49,6 +54,107 @@ var EMAILSENTRESPAGENT = [
 //#endregion
 
 //#region PUBLIC METHODS
+//#region PUBLIC AGENT
+AOS.prototype.handleAgentFindRequest = function (sessionAttrs) {
+    var deferred = q.defer();
+    var agentFindSpeechResp = new SpeechResponse();
+    var speechOutput = new Speech();
+    var repromptOutput = new Speech();
+
+    if (sessionAttrs.zip) {
+        getFinalAgentFindResponse(sessionAttrs)
+            .then(function (agentSpeechOutput) {
+                agentFindSpeechResp.speechOutput = agentSpeechOutput;
+                agentFindSpeechResp.repromptOutput = null;
+                agentFindSpeechResp.sessionAttrs = sessionAttrs;
+                deferred.resolve(agentFindSpeechResp);
+            });
+    } else {
+        speechOutput.text = Utilities.GetRandomValue(AGENTFINDRESP);
+        repromptOutput.text = Utilities.GetRandomValue(AGENTFINDRESP);
+        agentFindSpeechResp.speechOutput = speechOutput;
+        agentFindSpeechResp.repromptOutput = repromptOutput;
+        deferred.resolve(agentFindSpeechResp);
+    }
+
+    return deferred.promise;
+};
+
+AOS.prototype.handleAgentFindByZipIntent = function (sessionAttrs) {
+    var deferred = q.defer();
+    var agentFindSpeechResp = new SpeechResponse();
+    var speechOutput = new Speech();
+    var repromptOutput = new Speech();
+
+
+    getFinalAgentFindResponse(sessionAttrs)
+        .then(function (agentSpeechOutput) {
+            agentFindSpeechResp.speechOutput = agentSpeechOutput;
+            agentFindSpeechResp.repromptOutput = agentSpeechOutput;
+            agentFindSpeechResp.sessionAttrs = sessionAttrs;
+            deferred.resolve(agentFindSpeechResp);
+        });
+
+    return deferred.promise;
+}
+
+AOS.prototype.handleAgentFindEmailYesIntent = function (sessionAttrs) {
+    var deferred = q.defer();
+    var agentFindSpeechResp = new SpeechResponse();
+    var speechOutput = new Speech();
+    var repromptOutput = new Speech();
+
+    if (sessionAttrs.email) {
+        getFinalAgentFindSendEmailResponse(sessionAttrs)
+            .then(function (agentSpeechOutput) {
+                agentFindSpeechResp.speechOutput = agentSpeechOutput;
+                agentFindSpeechResp.repromptOutput = null;
+                deferred.resolve(agentFindSpeechResp);
+            });
+    } else {
+        speechOutput.text = Utilities.GetRandomValue(EMAILRESP);
+        repromptOutput.text = Utilities.GetRandomValue(EMAILRESP);
+        agentFindSpeechResp.speechOutput = speechOutput;
+        agentFindSpeechResp.repromptOutput = repromptOutput;
+        deferred.resolve(agentFindSpeechResp);
+    }
+
+
+    return deferred.promise;
+};
+
+AOS.prototype.handleAgentFindEmailNoIntent = function (sessionAttrs) {
+    var deferred = q.defer();
+    var agentFindSpeechResp = new SpeechResponse();
+    var speechOutput = new Speech();
+
+    speechOutput.text = "Thank you for chosing Allstate. You are in Good Hands.";
+    agentFindSpeechResp.speechOutput = speechOutput;
+    agentFindSpeechResp.repromptOutput = null;
+    deferred.resolve(agentFindSpeechResp);
+
+    return deferred.promise;
+};
+
+AOS.prototype.handleAgentFindEmailSendIntent = function (sessionAttrs) {
+    var deferred = q.defer();
+    var agentFindSpeechResp = new SpeechResponse();
+    var speechOutput = new Speech();
+    var repromptOutput = new Speech();
+
+    getFinalAgentFindSendEmailResponse(sessionAttrs)
+        .then(function (agentSpeechOutput) {
+            agentFindSpeechResp.speechOutput = agentSpeechOutput;
+            agentFindSpeechResp.repromptOutput = null;
+            deferred.resolve(agentFindSpeechResp);
+        });
+
+
+    return deferred.promise;
+
+};
+//#endregion
+
 
 AOS.prototype.handleRentersInsuranceStart = function (sessionAttrs) {
     var deferred = q.defer();
@@ -1981,6 +2087,73 @@ function initializeRentersInfoRequest() {
     rentersInfo.isSuccess = true;
     return rentersInfo;
 }
+//#endregion
+
+//#region PRIVATE AGENT
+function getFinalAgentFindSendEmailResponse(sessionAttrs) {
+    var deferred = q.defer();
+    var finalSpeechOutput = new Speech();
+    var to = sessionAttrs.email;
+    var subject = "Allstate agent details: " + sessionAttrs.agent.name;
+    var body = buildAgentEmailBody(sessionAttrs.agent, to);
+    Utilities.sendEmail(to, subject, body)
+        .then(function (emailStatus) {
+            if (emailStatus) {
+                finalSpeechOutput.text = Utilities.GetRandomValue(EMAILSENTRESPAGENT) + "Thank you, for choosing Allstate.";
+            } else {
+                finalSpeechOutput.text = "Sorry! there was a problem while sending the email to you. Please try again later.";
+            }
+            deferred.resolve(finalSpeechOutput);
+        })
+
+
+    return deferred.promise;
+}
+
+function buildAgentEmailBody(agentInfo, to) {
+    var emailBody = "";
+
+    emailBody = emailBody + "\nThank you for your interest in Allstate agents.\n"
+    emailBody = emailBody + "\nBelow are details you requested regarding our agent: " + agentInfo.name;
+    emailBody = emailBody + "\n-------------------------------------------------------";
+    emailBody = emailBody + "\nAdderess: " + Utilities.getCombinedAddress(agentInfo);
+    emailBody = emailBody + "\nPhone: " + agentInfo.phoneNumber;
+    emailBody = emailBody + "\nEmail: " + agentInfo.emailAddress;
+
+    return emailBody;
+}
+
+function getFinalAgentFindResponse(sessionAttrs) {
+    var deferred = q.defer();
+    var finalSpeechOutput = new Speech();
+    var sessionInfo = new Session();
+    sessionInfo.zip = sessionAttrs.zip;
+
+    startAOSSession()
+        .then(function (id) {
+            sessionInfo.sessionId = id;
+            return getStateFromZip(sessionInfo.sessionId, sessionInfo.zip);
+        }).then(function (state) {
+            sessionInfo.state = state;
+            return getAgents(sessionInfo);
+        }).then(function (agentsResp) {
+            if (agentsResp && agentsResp.length > 0) {
+                sessionAttrs.agent = agentsResp[0];
+                var firstAgentName = agentsResp[0].name;
+                finalSpeechOutput.text = "nearest Allstate agent to you is, " + firstAgentName +
+                    ". You can call the agent at " + agentsResp[0].phoneNumber +
+                    ". Or, would you like me to email you the agent details?";
+            } else {
+                finalSpeechOutput.text = "sorry! no agents are available at zip " + sessionInfo.zip;
+            }
+            deferred.resolve(finalSpeechOutput);
+        }).catch(function (error) {
+            finalSpeechOutput.text = "something went wrong with agent service. Please try again later.";
+            deferred.resolve(finalSpeechOutput);
+        });
+
+    return deferred.promise;
+};
 //#endregion
 
 
