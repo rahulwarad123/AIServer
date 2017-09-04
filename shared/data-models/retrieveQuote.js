c338c06a5aeb00e26ae7f809788f42c4f0e3a8f6
rahulwarad123 <rahul.warad@gmail.com>
2017-09-04 16:17:50 +0530

    adding retrieve quote model

---
+++ shared/data-models/retrieveQuote.js
@@ -0,0 +1,10 @@
+module.exports = function () {    
+    this.policyNumber;
+    this.controlNumber;
+    this.product;
+    this.startDate;
+    this.expiryDate;
+    this.agentName;
+    this.agentPhoneNumber;
+    this.agentEmailAddress;
+};
